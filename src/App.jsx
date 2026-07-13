88bd7d859cfc5a81701350cf888c3353600fc6fb
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-07-13 16:31:40 +0200

    Start login page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Dashborad from "../src/pages/Dashboard/Dashboard"
 
 export default function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn,  setIsLoggedIn] = useState(false)
 
 
 
 
   return (
     <Routes>
-      <Route path='/' element={<Login setIsLoggedIn={isLoggedIn}/>} />
+      <Route path='/' element={<Login  setIsLoggedIn={ setIsLoggedIn}/>} />
       <Route path='dashboard' element={<Dashborad/>} />
     </Routes>
   )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Form, Link } from 'react-router-dom'
+import appLogo from "../../assets/appLogo.png"
+import loginPicDog from "../../assets/loginPicDog.jpg"
+import { useNavigate } from 'react-router-dom'
+import styles from "./Login.module.css";
 
-export default function Login({isLoggedIn}) {
+export default function Login({ setIsLoggedIn}) {
 
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const navigate = useNavigate()
+
+    function handleLogin(event){
+
+        event.preventDefault()
+
+        setIsLoggedIn(true)
+        navigate("/dashboard")
+
+    }
 
 
 
   return (
-    <div>
+    <main className={styles.WraperLogin}>
 
-        <h1>Login</h1>
+        <div className={styles.mainContainerLogin}>
 
-       
+            {/* left side og login page */}
+            <section className={styles.loginLeftSideImage}>
+                <img src={appLogo} alt="logo" width={400} />
+                <img className={styles.dogImg} src={loginPicDog} alt="Dog's best friend" />
+            </section>
 
-    </div>
+            {/* right side of login page */}
+            <section className={styles.loginRightSide}>
+                <form onSubmit={handleLogin} className={styles.loginForm}>
+                    <img src={appLogo} alt="logo" width={200} />
+                    <input type="mail" placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)}/>
+                    <input type="password" placeholder='Password' value={password} onChange={(event) => setPassword(event.target.value)}/>
+                    <button type='submit'>Loggin</button>
+                </form>
+            </section>
+
+        </div>
+
+    </main>
   )
 }
