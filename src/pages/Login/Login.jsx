cd8c41c1b96b88485e02c4c0bbb119f563ca7460
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-07-13 13:30:00 +0200

    Create project structure

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Login({isLoggedIn}) {
+
+
+
+
+
+  return (
+    <div>
+
+        <h1>Login</h1>
+
+       
+
+    </div>
+  )
+}
