54201bcffafe7acce7703b635629bf3383ca541e
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-07-26 19:42:44 +0200

    modal styling started

--- src/components/Modals/AddAnimalModal/AddAnimalModal.jsx
+++ src/components/Modals/AddAnimalModal/AddAnimalModal.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+
+import styles from "./AddAnimalModal.module.css"
+
 
 export default function AddAnimalModal({ onClose }) {
   return (
-    <div>
-      <h2>Add animal</h2>
+    <div className={styles.addAnimalMainContainer}>
 
-      <button type="button" onClick={onClose}>
-        Close
-      </button>
+      <div className={styles.addAnimalContainer}>
+
+        <h1>Add animal</h1>
+
+        <p>
+          Fill in the details below to create anew animal
+        </p>
+
+        <form className={styles.addAnimalForm} action="">
+
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" required/>
+
+          <label htmlFor="breed">Breed</label>
+          <input type="text" id="breed" required/>
+
+          <label htmlFor="age">Age</label>
+          <input type="text" id="age" />
+
+          <label htmlFor="gender">Gender</label>
+          <input type="text" id="gender" />
+
+          <label htmlFor="weight">Weight (kg)</label>
+          <input type="text" id="weight" />
+
+          <label htmlFor="color">Color</label>
+          <input type="text" id="color" />
+
+          <label htmlFor="vaccinated">Vaccinated</label>
+          <input type="checkbox" id="vaccinated" />
+
+          <label htmlFor="neutered">Neutered</label>
+          <input type="checkbox" id="neutered" />
+
+          <label htmlFor="date">Date added</label>
+          <input type="date" id="date" />
+
+
+
+
+
+
+          <button onClick={onClose}>Cancel</button>
+          <button type="submit" >Save animal</button>
+
+        </form>
+
+        
+
+      </div>
+
     </div>
   );
 }
