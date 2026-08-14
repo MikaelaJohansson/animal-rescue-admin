86249faaa1d7943aa4fb085a9fa8f8296ad6da5e
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-08-14 14:07:41 +0200

    detail adoption page base

--- src/pages/Adoptions/AdoptionDetails/AdoptionDetails.jsx
+++ src/pages/Adoptions/AdoptionDetails/AdoptionDetails.jsx
@@ -3,6 +3,8 @@ import {db} from "../../../firebase"
 import { getDoc, doc } from "firebase/firestore";
 import {useParams, Link} from 'react-router-dom'
 import styles from "./AdoptionDetails.module.css"
+import StatusBadge from "../../../components/StatusBadge/StatusBadge"
+import animalImages from "../../../Data/animalImages";
 
 
 export default function AdoptionDetails() {
@@ -59,12 +61,45 @@ export default function AdoptionDetails() {
         return <p>{errorMessage}</p>
     }
 
+    const animalImage = animalImages[applicationData.animalImage];
+
     
   return (
-    <div>
-        AdoptionDetails <br />
-        <Link to={"/adoptions"}>back to aplications</Link> <br />
-        {applicationData.animalName}
+    <div className={styles.adoptionDetailsMainContainer}>
+
+        <p className={styles.adoptionDetailsLink}>
+            <Link to={"/adoptions"}>back to aplications</Link> <br />
+        </p>
+        
+       
+        <div className={styles.adoptionDetailsContainer}>
+
+            <div className={styles.adoptionDetailsApplicant}>
+                <h2>Applicant information</h2>
+                <p>
+                    Name: {applicationData.applicantName}
+                    Email: {applicationData.email}
+                    Phone: {applicationData.phone}
+
+                </p>
+            </div>
+
+            <div className={styles.adoptionDetailsRequest}>
+                <h2>Requested animal</h2>
+                    <img src={animalImage} alt={applicationData.animalName}/>
+                    {applicationData.animalName}
+            </div>
+
+            <div className={styles.adoptionDetailsDetails}>
+                <h2>Application Detils</h2>
+            </div>
+
+            <div className={styles.adoptionDetailsStatus}>
+                <h2>Status</h2>
+               
+                <StatusBadge status={applicationData.status}/>
+            </div>
+        </div>
 
     </div>
   )
