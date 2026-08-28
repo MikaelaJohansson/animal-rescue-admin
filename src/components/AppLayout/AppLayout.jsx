44b89f9df0dda5a51c421ac4eb34e11e6f4a10bb
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-08-28 20:12:55 +0200

    style fix z-index

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/Sidebar'
-import Topbar from "../TopBar/Topbar";
+import Topbar from "../Topbar/Topbar";
 import styles from "../AppLayout/AppLayout.module.css"
 
 
