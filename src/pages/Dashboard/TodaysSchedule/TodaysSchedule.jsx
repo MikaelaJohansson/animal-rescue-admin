b6bf607dd0ab9abbe195185105a7f74bb5921cf1
Mikaela Johansson <mikaela.johansson87@gmail.com>
2026-09-01 13:23:42 +0200

    styled calendar events dachboard

--- src/pages/Dashboard/TodaysSchedule/TodaysSchedule.jsx
+++ src/pages/Dashboard/TodaysSchedule/TodaysSchedule.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from "./TodaysSchedule.module.css"
 import { Link } from "react-router-dom"
+import { LuCalendarDays, LuArrowRight  } from "react-icons/lu";
+import backgroundDog from "../../../assets/backgroundDog.png"
 
 export default function TodaysSchedule({todayEvents}) {
 
@@ -9,36 +11,47 @@ export default function TodaysSchedule({todayEvents}) {
     return (
         <section className={styles.scheduleContainer}>
 
-            <h2>Today's Events</h2>
+            <h2> <LuCalendarDays /> Today's Events</h2>
 
             <div className={styles.scheduleList}>
 
                 {todayEvents.length > 0 ? (
 
-                todayEvents.map((event) => {
+                    todayEvents.map((event) => {
 
-                    return (
+                        return (
+                            <Link key={event.id} to="/calendar" className={styles.scheduleEvent} >
 
-                        <Link
-                            key={event.id}
-                            to="/calendar"
-                            className={styles.scheduleEvent}
-                        >
-                            <span className={styles.scheduleTime}>
-                                {event.time}
-                            </span>
+                                <span className={styles.scheduleTime}>
+                                    {event.time}
+                                </span>
 
-                            <span>
-                             {event.title}
-                            </span>
-                        </Link>
-                    )
+                                <span  className={styles.scheduleEventIcon}><LuCalendarDays /></span>
 
-                })) : (
+                                <span className={styles.scheduleEventTitle}>
+                                    {event.title}
+                                </span>
 
-                    <p>Nothing scheduled for today.</p>
+                            </Link>
+                        )
+                    })
+
+                ) : (
+
+                    <div className={styles.scheduleContainerNoEvent}>
+
+                        <img src={backgroundDog} alt="" />
+                        <h3>Nothing scheduled for today.</h3>
+                        <p>Enjoy a calm day!</p>
+
+                        <hr />
+                    </div>
 
                 )}
+
+                <Link to="/calendar" className={styles.calendarLink} >
+                    View full calendar <LuArrowRight />
+                </Link>
 
             </div>
 
